c2507e13b5b7244d4992977533d770743efca591
Lyudmyla Tkachenko <luda_tkach@yahoo.com>
2020-12-30 23:01:11 -0800

    Refactoring selectors in Getnada.page.js

--- test/pageobjects/Getnada.page.js
+++ test/pageobjects/Getnada.page.js
@@ -3,11 +3,11 @@ import BasePage from './Base.page';
 
 class GetnadaPage extends BasePage {
 
-  get addInbox() { return browser.$('//i[@class="icon-plus"]');}
+  get addInbox() { return browser.$('//li[@class="items-center"]//button');} // '//i[@class="icon-plus"]');}
 
-  get userName() { return browser.$('//input[@class="user_name"]');}
+  get userName() { return browser.$('//input[@id="grid-first-name"]');} // //input[@class="user_name"]');}
 
-  get acceptButton() { return browser.$('//a[@class="button success"]');}
+  get acceptButton() { return browser.$('//form/button[@type="button"]');}    // //a[@class="button success"]');}
 
   get activeEmail() { return browser.$('//a[@class=" is-active"]//span');}
 
